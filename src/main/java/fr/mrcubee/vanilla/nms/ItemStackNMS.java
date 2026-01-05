da5cf983542729c19aabd3fe17bc6f9ab4f3917b
MrCubee <contact@mrcubee.fr>
2026-01-05 15:03:06 +0100

    style(nms): fix indentation and remove accidental leading whitespace
    - Remove extra tabs/spaces that were incorrectly added to the beginning of every line in ItemStackNMS.java
    - Normalize file formatting to standard Java conventions

--- src/main/java/fr/mrcubee/vanilla/nms/ItemStackNMS.java
+++ src/main/java/fr/mrcubee/vanilla/nms/ItemStackNMS.java
@@ -1,34 +1,33 @@
-    package fr.mrcubee.vanilla.nms;
+package fr.mrcubee.vanilla.nms;
 
-    import org.bukkit.block.Block;
-    import org.bukkit.craftbukkit.v1_21_R7.CraftWorld;
-    import org.bukkit.craftbukkit.v1_21_R7.block.CraftBlock;
-    import org.bukkit.craftbukkit.v1_21_R7.entity.CraftPlayer;
-    import org.bukkit.craftbukkit.v1_21_R7.inventory.CraftItemStack;
-    import org.bukkit.entity.Player;
-    import org.bukkit.inventory.ItemStack;
+import org.bukkit.block.Block;
+import org.bukkit.craftbukkit.v1_21_R7.CraftWorld;
+import org.bukkit.craftbukkit.v1_21_R7.block.CraftBlock;
+import org.bukkit.craftbukkit.v1_21_R7.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_21_R7.inventory.CraftItemStack;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
-    public class ItemStackNMS {
+public class ItemStackNMS {
 
-        public static boolean mineBlock(final Player player, final Block block, final ItemStack itemStack) {
-            final CraftPlayer craftPlayer;
-            final CraftBlock craftBlock;
-            final net.minecraft.world.item.ItemStack nmsItemStack;
+    public static boolean mineBlock(final Player player, final Block block, final ItemStack itemStack) {
+        final CraftPlayer craftPlayer;
+        final CraftBlock craftBlock;
+        final net.minecraft.world.item.ItemStack nmsItemStack;
 
-            if (player == null || block == null)
-                return false;
-            craftPlayer = (CraftPlayer) player;
-            craftBlock = (CraftBlock) block;
-            if (itemStack != null) {
-                nmsItemStack = CraftItemStack.asNMSCopy(itemStack);
-                nmsItemStack.a(((CraftWorld) block.getWorld()).getHandle(), craftBlock.getNMS(), craftBlock.getPosition(), craftPlayer.getHandle());
-                block.breakNaturally(itemStack);
-                itemStack.setItemMeta(CraftItemStack.getItemMeta(nmsItemStack));
-                return nmsItemStack.f();
-            } else {
-                block.breakNaturally();
-                return false;
-            }
+        if (player == null || block == null)
+            return false;
+        craftPlayer = (CraftPlayer) player;
+        craftBlock = (CraftBlock) block;
+        if (itemStack != null) {
+            nmsItemStack = CraftItemStack.asNMSCopy(itemStack);
+            nmsItemStack.a(((CraftWorld) block.getWorld()).getHandle(), craftBlock.getNMS(), craftBlock.getPosition(), craftPlayer.getHandle());
+            block.breakNaturally(itemStack);
+            itemStack.setItemMeta(CraftItemStack.getItemMeta(nmsItemStack));
+            return nmsItemStack.f();
+        } else {
+            block.breakNaturally();
+            return false;
         }
-
     }
+}
